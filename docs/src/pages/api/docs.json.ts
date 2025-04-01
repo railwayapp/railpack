8b2707311e9d3f1f9e4f4a12505734070aba0f9c
Nicolas Theck <contact@itsrems.com>
2025-04-01 17:32:23 +0200

    no auth check

--- docs/src/pages/api/docs.json.ts
+++ docs/src/pages/api/docs.json.ts
@@ -1,22 +1,22 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
-import { randomBytes } from "crypto";
+// import { randomBytes } from "crypto";
 
-export const prerender = false;
+// export const prerender = false;
 
 export const GET: APIRoute = async (ctx) => {
-  const secret =
-    // @ts-ignore import.meta is throwing type errors
-    import.meta.env.EXPORT_ENDPOINT_PASSWORD ?? randomBytes(16).toString("hex");
+  // const secret =
+  //   // @ts-ignore import.meta is throwing type errors
+  //   import.meta.env.EXPORT_ENDPOINT_PASSWORD ?? randomBytes(16).toString("hex");
 
-  if (ctx.request.headers.get("Authorization") !== secret) {
-    return new Response("Unauthorized", {
-      status: 401,
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    });
-  }
+  // if (ctx.request.headers.get("Authorization") !== secret) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //     headers: {
+  //       "Content-Type": "text/plain",
+  //     },
+  //   });
+  // }
 
   const docsEntries = await getCollection("docs");
 
