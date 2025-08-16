3569d360cdfa620510a664112545c05c4e6f6b50
Michael Bianco <mike@mikebian.co>
2025-08-16 15:29:38 -0600

    test: added scripts to generate RR from the latest templates

--- examples/node-vite-react-router-spa/generate.sh
+++ examples/node-vite-react-router-spa/generate.sh
@@ -1,12 +1,9 @@
 # Description: autogen this repo from the latest version of the RR template
 
-pnpx create-react-router@latest . --yes
-
-rm Dockerfile
-rm -rf .git
+pnpx create-react-router@latest . --yes --overwrite --no-git-init
 
 # remove the start script otherwise it won't be treated as an SPA
 yq eval -i 'del(.scripts.start)' package.json
 
-# disable SSR for SPA build
+# disable SSR for SPA buildg st
 sed -i '' 's/ssr: true/ssr: false/' react-router.config.ts
