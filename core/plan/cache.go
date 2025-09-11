60435f4cc095c43364f9018a6bb42394e12bd3fb
Michael Bianco <mike@mikebian.co>
2025-09-11 06:40:37 -0600

    refactor: remove obvious comments

--- core/plan/cache.go
+++ core/plan/cache.go
@@ -6,11 +6,8 @@ const (
 )
 
 type Cache struct {
-	// The directory to cache
 	Directory string `json:"directory,omitempty" jsonschema:"description=The directory to cache"`
-
-	// The type of cache (either "shared" or "locked")
-	Type string `json:"type,omitempty" jsonschema:"enum=shared,enum=locked,default=shared,description=The type of cache (either 'shared' or 'locked')"`
+	Type      string `json:"type,omitempty" jsonschema:"enum=shared,enum=locked,default=shared,description=The type of cache (either 'shared' or 'locked')"`
 }
 
 func NewCache(directory string) *Cache {
