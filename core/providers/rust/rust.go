16eda5963091283ca5a504385f4514fe19c943ff
Michael Bianco <mike@mikebian.co>
2025-10-21 14:38:10 -0600

    fix: remove unneeded cat in rust provider

--- core/providers/rust/rust.go
+++ core/providers/rust/rust.go
@@ -168,7 +168,6 @@ func (p *RustProvider) Install(ctx *generate.GenerateContext, install *generate.
 	install.AddCommands([]plan.Command{
 		plan.NewExecCommand(`mkdir -p src`),
 		plan.NewExecShellCommand(dummyCmd, plan.ExecOptions{CustomName: "compile dependencies"}),
-		plan.NewExecCommand(`cat /app/src/main.rs`),
 		plan.NewExecCommand(fmt.Sprintf("%s%s", buildCmd, targetArg)),
 		plan.NewExecCommand(fmt.Sprintf("rm -rf src target/%srelease/%s*", targetPath, p.getAppName(ctx))),
 	})
