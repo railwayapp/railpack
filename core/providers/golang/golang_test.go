3fa201c7f3b50fe4737664d2d6191c570f1e99a2
Michael Bianco <mike@mikebian.co>
2025-11-11 15:49:38 -0700

    test: fix go version mismatch

--- core/providers/golang/golang_test.go
+++ core/providers/golang/golang_test.go
@@ -37,7 +37,7 @@ func TestGolang(t *testing.T) {
 			detected:     true,
 			hasGoMod:     false,
 			hasWorkspace: true,
-			goVersion:    "1.25",
+			goVersion:    "1.25.3",
 		},
 		{
 			name:     "node",
