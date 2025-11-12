7dba280cee50e8a09a3273226531323cae5f25e2
Michael Bianco <mike@mikebian.co>
2025-11-11 21:00:34 -0700

    feat: respect go version specified by mise (#300)
    * fix: respect mise go version in go provider
    
    * test: assert against specific go version
    
    * test: snapshot update
    
    * build: trust all mise.toml files during setup
    
    * ci: trust all mise.toml before running unit tests
    
    * test: require exact version in go project output
    
    * Revert "ci: trust all mise.toml before running unit tests"
    
    This reverts commit e6994b32fb6b168a4170c14ace20ebf07aea8b67.
    
    * test: split test into two cases
    
    * test: specify non-default go version to assert against
    
    * test: snapshot update
    
    * test: fix go version mismatch
    
    * fixup! test: fix go version mismatch
    
    * build: we don't need the trust script to run our tests

--- examples/go-workspaces/shared/shared.go
+++ examples/go-workspaces/shared/shared.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("This is the shared package")
+	fmt.Printf("This is the shared package %s\n", runtime.Version())
 }
