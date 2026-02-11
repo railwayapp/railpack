2853722b4dc71d5c708e9b963e38b3755c98a38a
Ray Chen <ray@raychen.io>
2026-02-11 09:43:23 +0800

    feat(python): Scan nested dependency files in usesDep

--- core/providers/python/python.go
+++ core/providers/python/python.go
@@ -465,11 +465,16 @@ func (p *PythonProvider) addMetadata(ctx *generate.GenerateContext) {
 }
 
 func (p *PythonProvider) usesDep(ctx *generate.GenerateContext, dep string) bool {
-	for _, file := range []string{"requirements.txt", "pyproject.toml", "Pipfile"} {
-		if contents, err := ctx.App.ReadFile(file); err == nil {
-			// TODO: Do something better than string comparison
-			if strings.Contains(strings.ToLower(contents), strings.ToLower(dep)) {
-				return true
+	for _, pattern := range []string{"**/requirements.txt", "**/pyproject.toml", "**/Pipfile"} {
+		files, err := ctx.App.FindFiles(pattern)
+		if err != nil {
+			continue
+		}
+		for _, file := range files {
+			if contents, err := ctx.App.ReadFile(file); err == nil {
+				if strings.Contains(strings.ToLower(contents), strings.ToLower(dep)) {
+					return true
+				}
 			}
 		}
 	}
