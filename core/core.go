7cea8312a2af7c16a76de5bff33c85bf0650c7be
Michael Bianco <mike@mikebian.co>
2026-07-30 07:25:26 -0600

    refactor(core): inline mergeSecrets helper function
    Generated-by: aiautocommit

--- core/core.go
+++ core/core.go
@@ -159,13 +159,9 @@ func GetConfig(app *app.App, env *app.Environment, options *GenerateBuildPlanOpt
 
 	mergedConfig := c.Merge(optionsConfig, envConfig, fileConfig)
 	// Environment-provided secrets must remain available when file configuration replaces slice values.
-	mergedConfig.Secrets = mergeSecrets(envConfig.Secrets, mergedConfig.Secrets)
+	mergedConfig.Secrets = utils.RemoveDuplicates(slices.Concat(envConfig.Secrets, mergedConfig.Secrets))
 
 	return mergedConfig, nil
-}
-
-func mergeSecrets(envSecrets []string, mergedConfigSecrets []string) []string {
-	return utils.RemoveDuplicates(slices.Concat(envSecrets, mergedConfigSecrets))
 }
 
 func GenerateConfigFromFile(app *app.App, env *app.Environment, options *GenerateBuildPlanOptions, logger *logger.Logger) (*c.Config, error) {
